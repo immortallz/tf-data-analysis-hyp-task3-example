547253e6156a11781ce5d1ce9c99659e1b0ae04b
immortallz <129889191+immortallz@users.noreply.github.com>
2023-04-11 23:50:39 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,7 +5,7 @@ import numpy as np
 chat_id = 388568881 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    if 1.04 * x.mean() > 500:
+    if 1.04 * np.median(x) > 500:
       return False
     else:
       return True
