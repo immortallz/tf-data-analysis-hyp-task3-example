c52175025dc7b8d635ab239eb9e54c30afe6d419
immortallz <129889191+immortallz@users.noreply.github.com>
2023-04-12 22:56:57 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,12 @@
 import pandas as pd
 import numpy as np
 
+from statsmodels.stats.weightstats import ztest
 
 chat_id = 388568881 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    if 1.04 * np.median(x) > 500:
-      return False
+    if ztest(x, value = 500, alternative = 'larger')[1] < 0.04:
+        return False
     else:
-      return True
+        return True
