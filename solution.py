90e1507e9312834bdd079613a8399f379400f5a1
immortallz <129889191+immortallz@users.noreply.github.com>
2023-04-13 17:32:58 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -6,7 +6,7 @@ from statsmodels.stats.weightstats import ztest
 chat_id = 388568881 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    if ztest(x, value = 500, alternative = 'larger')[1] < 0.04:
+    if ztest(x, value = 500, alternative = 'larger')[1] > 0.04:
         return False
     else:
         return True
